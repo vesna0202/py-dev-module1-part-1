9e94de2a2063cae3ba15afc6ef10b9fe5ab9fec0
vesna0202 <vesnab0409@gmail.com>
2023-11-21 19:49:37 +0000

    added demo files

---
+++ demo1.py
@@ -0,0 +1,10 @@
+print("Dobrodošli u PyDev program obrazovanja")
+
+first_name = 'Pero'
+last_name = 'Perić'
+date_of_birth = 1990 #1.998,00
+mobile_phone = '+385 9n 1234-567'
+employed = True
+height = 180.99
+
+print(first_name, last_name)
