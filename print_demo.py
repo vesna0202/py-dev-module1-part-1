d653d8e55da782675329a95d46a635ac951979f3
vesna0202 <vesnab0409@gmail.com>
2023-11-23 20:15:58 +0000

    Added print demo

---
+++ print_demo.py
@@ -0,0 +1,12 @@
+first_name = 'Pero'
+last_name = 'Perić'
+phone = '+385 9_ 1234 567'
+
+#print()
+#print(first_name, end=' ')
+#print(first_name, last_name, phone)
+
+#print(first_name, last_name, phone, sep=' - ; - ')
+
+print(first_name, end=' ')
+print(last_name, phone, sep=', ', end='\n')
